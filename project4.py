78a6f6959e4579a405ae67dce671d6399dfe65d9
hanyh m <hanyhmoll46@gmail.com>
2025-03-08 08:37:14 +0330

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,10 @@
+score = float(input("لطفا نمره دانش‌آموز را وارد کنید: "))
+
+if score >= 90:
+    print("عالی")
+elif score >= 70:
+    print("خوب")
+elif score >= 50:
+    print("قابل قبول")
+else:
+    print("مردود")
