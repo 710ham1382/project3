173e60f04e58659ff6953680325d2eec4a940264
hanyh m <hanyhmoll46@gmail.com>
2025-03-08 08:34:16 +0330

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,6 @@
+
+char = input("Please enter: ")
+if char in ['a', 'e', 'i', 'o', 'u']:
+                print(f"{char}This word is sonorous ")
+else:
+                  print(f"{char} This word is not sonorous")
